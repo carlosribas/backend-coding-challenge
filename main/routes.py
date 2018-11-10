982a6af89a0b0d4ddc53a80aadf1bd192a86406a
Carlos Eduardo Ribas <caduribas@gmail.com>
2018-11-10 19:51:51 -0200

    Using Docstring

--- main/routes.py
+++ main/routes.py
@@ -11,15 +11,24 @@ from worker import conn
 translation_queue = Queue(connection=conn)
 
 
-def create_translation(id, text):
-    text_to_be_translated = Translator.query.get(id)
+def create_translation(text_id, text):
+    """HTTP request to the translation endpoint. Saves the uid.
+
+    :param text_id: id from the new_text instance created in home()
+    :param text: text to be translated
+    """
+    text_to_be_translated = Translator.query.get(text_id)
     post_text = post_translation(text)
     text_to_be_translated.uid = post_text.uid
     db.session.commit()
 
 
-def update_translation(id):
-    text = Translator.query.get(id)
+def update_translation(text_id):
+    """Check the translation status. When there are changes, save status and/or translated text.
+
+    :param text_id: id from the new_text instance created in home()
+    """
+    text = Translator.query.get(text_id)
     time.sleep(10)
     get_text = get_translation(text.uid)
 
@@ -31,14 +40,15 @@ def update_translation(id):
     elif get_text.status == 'translating':
         text.status = 'pending'
         db.session.commit()
-        update_translation(id)
+        update_translation(text_id)
 
     elif get_text.status == 'new':
-        update_translation(id)
+        update_translation(text_id)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
+    """Main function showing translated texts and translation requests."""
     form = TranslatorForm()
     texts = Translator.query.order_by(func.length(Translator.text_translated))
 
@@ -49,8 +59,13 @@ def home():
         )
         db.session.add(new_text)
         db.session.commit()
+
+        #: Delay execution of create_translation
         new_request = translation_queue.enqueue_call(create_translation, args=(new_text.id, new_text.text,))
+
+        # Delay execution of update_translation. This job will be queued when "new_request" is completed
         translation_queue.enqueue(update_translation, args=(new_text.id,), depends_on=new_request)
+
         flash('Your text will be translated. Please wait.', 'success')
         return redirect(url_for('home'))
 
--- worker.py
+++ worker.py
@@ -3,7 +3,9 @@ import redis
 from rq import Worker, Queue, Connection
 from config import load_dotenv
 
+#: Tell RQ what Redis connection to use
 conn = redis.from_url(os.environ['REDIS_URL'])
+#: Name of the default queue
 listen = ['default']
 
 if __name__ == '__main__':
