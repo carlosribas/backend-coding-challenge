3e001fa0c9d62f7342df28ef19254284b20479ee
Carlos Eduardo Ribas <caduribas@gmail.com>
2018-11-11 14:06:44 -0200

    Using Docstring

--- unbabel_api.py
+++ unbabel_api.py
@@ -3,17 +3,25 @@ from unbabel.api import UnbabelApi
 from config import load_dotenv
 
 uapi = UnbabelApi(os.environ['USERNAME'], os.environ['API_KEY'], sandbox=True)
-callback_url = 'http://127.0.0.1/unbabel_callback/'
 
 
 def post_translation(text):
+    """HTTP request to the translation endpoint.
+
+    :param text: text to be translated
+    :return: dict with the uid
+    """
     return uapi.post_translations(
         text=text,
         source_language='en',
-        target_language='es',
-        callback_url=callback_url
+        target_language='es'
     )
 
 
 def get_translation(uid):
+    """Check the translation status.
+
+    :param uid: uid from the text to be translated
+    :return: dict with status and translatedText (if the translation is completed)
+    """
     return uapi.get_translation(uid)
